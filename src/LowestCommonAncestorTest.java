8ccb5372f407f21b768c5be32e5290c59c5f76f2
YasirZardari <yasirzardari@gmail.com>
2017-10-14 23:50:13 +0100

    Added more tests, still failing

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -7,19 +7,27 @@ public class LowestCommonAncestorTest {
 	@Test
 	public void testLowestCommonAncestor() {
 		BST<Integer, Integer> bst = new BST<Integer, Integer>();
-//		bst.put(7, 7);  
-//        bst.put(8, 8);   
-//        bst.put(3, 3);  
-//        bst.put(1, 1);
-//        bst.put(2, 2);   
-//        bst.put(6, 6);   
+
 		
-		bst.put(2, 2);
-		bst.put(1, 1);
+		bst.put(7, 7);
+		bst.put(5, 5);
+		bst.put(12, 12);
 		bst.put(3, 3);
+		bst.put(6, 6);
+		bst.put(1, 1);
+		bst.put(4, 4);
+		bst.put(9, 9);
+		bst.put(8, 8);
+		bst.put(15, 15);
+		bst.put(10, 10);
+		bst.put(15, 15);
+		bst.put(13, 13);
+		bst.put(17, 17);
+
+		
         
-		//assertEquals(bst.get(7),bst.LCA(8, 9));
-		assertEquals(bst.get(2),bst.LCA(1, 3));
+		assertEquals(bst.get(7),bst.LCA(5, 12));
+		assertEquals(bst.get(12),bst.LCA(9, 17));
 	}
 
 }
